86f87cf05c07bebf49f1dd351d232024e38433b6
ngavinsir <ngavinsir@gmail.com>
2021-01-30 21:11:25 +0700

    extract IconButton component

--- src/components/sudoku/BottomPanel/BottomPanel.svelte
+++ src/components/sudoku/BottomPanel/BottomPanel.svelte
@@ -2,6 +2,7 @@
   import clsx from "clsx";
   import NumberSelector from "./NumberSelector.svelte";
   import { board } from "@/lib/Board";
+  import IconButton from "./IconButton.svelte";
 
   let className;
   export { className as class };
@@ -9,11 +10,8 @@
 
 <div class={clsx(className, 'flex flex-col space-y-5')}>
   <div class="flex">
-    <div
-      class="flex flex-col items-center justify-center p-3 space-y-2 cursor-pointer hover:bg-gray-700"
-      on:click={() => board.removeSelectedCell()}
-    >
-      <!-- Remove icon -->
+    <!-- Remove icon -->
+    <IconButton label="Remove">
       <svg
         class="w-6 h-6"
         fill="none"
@@ -28,8 +26,7 @@
           d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
         />
       </svg>
-      <span class="text-base select-none">Remove</span>
-    </div>
+    </IconButton>
   </div>
   <NumberSelector
     on:select-number={(event) => {
---
+++ src/components/sudoku/BottomPanel/IconButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let label: string = "";
+</script>
+
+<div
+  class="flex flex-col items-center justify-center p-3 space-y-2 cursor-pointer hover:bg-gray-700"
+  on:click
+>
+  <!-- Remove icon -->
+  <slot />
+  <span class="text-base select-none">{label}</span>
+</div>
